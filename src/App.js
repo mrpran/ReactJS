dfa9c80485d34060578a34a53e272bfde9827dff
mrpran <pran05412@gmail.com>
2021-05-22 10:33:08 +0530

    commit State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
-import Welcome from './components/Welcome';
+import Welcome from './components/Welcome'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
+
+
+      <Message />
+
       <Greet name="Bruce" heroName="Batman">
         <p>This is children props</p>
       </Greet>
@@ -17,6 +22,8 @@ function App() {
       <Welcome name="Bruce" heroName="Batman" />
       <Welcome name="Clark" heroName="Superman" />
       <Welcome name="Diana" heroName="Wonder Woman" />
+
+
     </div>
   );
 }
