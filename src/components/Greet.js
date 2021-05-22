7f1eff72a8f61038dae5e9cad04cbfd5df27f194
mrpran <pran05412@gmail.com>
2021-05-22 10:09:21 +0530

    Added Class component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -5,7 +5,10 @@ import React from 'react'
 // }
 
 
-const Greet = () => <h1>Hello Pran !</h1>
-
+const Greet = () => {
+    return (
+        <h1>Hello Pran !</h1>
+    )
+}
 
 export default Greet;
