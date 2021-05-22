9afd6c4e88660a4fe8fd0e157c15bcfcb6a749e0
mrpran <pran05412@gmail.com>
2021-05-22 10:22:13 +0530

    Props in class component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import Welcome from './components/Welcome';
 function App() {
   return (
     <div className="App">
-      <Greet name="Pran" heroName="thor"/>
-      <Welcome/>
+      <Greet name="Bruce" heroName="Batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Woman" />
+
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Superman" />
+      <Welcome name="Diana" heroName="Wonder Woman" />
     </div>
   );
-    }
-
-
-
+}
 
 export default App;
