a7aad04475b7a183023a817239413ff2a11b1782
mrpran <pran05412@gmail.com>
2021-05-25 19:22:57 +0530

    event handling in function and class component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 function App() {
   return (
     <div className="App">
 
 
+      <FunctionClick />
+      <ClassClick />
+      
+{/*       
       <Counter />
       <Message />
 
@@ -23,7 +29,7 @@ function App() {
 
       <Welcome name="Bruce" heroName="Batman" />
       <Welcome name="Clark" heroName="Superman" />
-      <Welcome name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Diana" heroName="Wonder Woman" /> */}
 
 
     </div>
